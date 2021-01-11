aaee09a29afab8173874f63b8018c8b95a293279
Alexandre Lordelo <alexandre.lordelo@icloud.com>
2021-01-11 16:22:34 +0100

    ProjectEdit: tests to edit name, still crashing

--- task-tracker-swiftui/Views/Project/ProjectEdit.swift
+++ task-tracker-swiftui/Views/Project/ProjectEdit.swift
@@ -6,10 +6,11 @@
 //
 
 import SwiftUI
+import RealmSwift
 
 struct ProjectEdit: View {
     
-   // let realm: Realm
+  // let realm: Realm
     
     var project: Project
 
@@ -28,15 +29,105 @@ struct ProjectEdit: View {
         TextField("Name", text: $name)
 
          }
+        
+        .toolbar {
+
+            //update button
+            ToolbarItem(placement: .confirmationAction) {
+                Button(action: {
+                    updateProject(updatedProject: project)
+                }) {
+                    Text("Done")
+                    .foregroundColor(.blue)
+                }
+
+            }
+
+            //cancel button
+            ToolbarItem(placement: .cancellationAction) {
+                Button(action: {
+                         self.presentationMode.wrappedValue.dismiss()
+                      }) {
+                        Text("Cancel")
+                      }
+
+            }
+        }
             
         .navigationBarTitle(project.name ?? "Workplace name")
 
         }
         .navigationViewStyle(StackNavigationViewStyle())
+        
+ 
 
         
         
     }
+    
+    
+    
+//    func updateWorkplace(updatedWorkplace: Workplace) {
+//      state.error = nil
+//      state.shouldIndicateActivity = true
+//
+//      do {
+//        try state.realmObject!.write {
+//            workplace.name = name
+//        }
+//        state.shouldIndicateActivity = false
+//        self.presentationMode.wrappedValue.dismiss()
+//
+//      } catch {
+//        state.error = "Unable to open Realm write transaction"
+//      }
+//    }
+    
+
+
+    
+    //update edited workspace to Realm
+
+    func updateProject(updatedProject: Project) {
+
+        state.error = nil
+       // state.shouldIndicateActivity = true
+
+        let realmConfig = app.currentUser?.configuration(partitionValue: project.partition!)
+
+        guard var config = realmConfig else {
+            state.error = "Internal error - cannot get Realm config"
+           // state.shouldIndicateActivity = false
+            return
+        }
+
+        config.objectTypes = [Project.self]
+
+        Realm.asyncOpen(configuration: config) { result in
+            switch result {
+            case .failure(let error):
+                DispatchQueue.main.async {
+                    self.state.error = "Couldn't open realm: \(error)"
+                //    state.shouldIndicateActivity = false
+                }
+                return
+            case .success(let realm):
+
+                do {
+                    try state.realmObject!.write {
+                        
+                        project.name = name
+
+                    //   state.shouldIndicateActivity = false
+                        self.presentationMode.wrappedValue.dismiss()
+                    }
+                } catch {
+                    state.error = "Unable to open Realm write transaction"
+                }
+            }
+        }
+    }
+
 }
 
 //struct ProjectEdit_Previews: PreviewProvider {
