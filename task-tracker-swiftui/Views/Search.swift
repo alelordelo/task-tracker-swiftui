567456032de8a19fd841bf09cc5017d14b633982
Alexandre Lordelo <alexandre.lordelo@icloud.com>
2021-01-14 20:20:12 +0100

    Search: tests, still no results

--- task-tracker-swiftui/Views/Search.swift
+++ task-tracker-swiftui/Views/Search.swift
@@ -37,7 +37,7 @@ struct Search: View {
                     Text(state.user!.name)
 
                     }
-                 .onAppear(perform: fetchData)
+                 .onAppear(perform: searchTasks)
              
           
             }
@@ -47,41 +47,47 @@ struct Search: View {
         }
 
 
-    private func fetchData() {
+    func searchTasks() {
+                        
+            // mongodb-atlas is the cluster service name
+        let client = app.currentUser!.mongoClient("mongodb-atlas")
 
-       // mongodb-atlas is the cluster service name
-       let client = app.currentUser!.mongoClient("mongodb-atlas")
+            // Select the database
+        let database = client.database(named: "tracker2")
 
-       // Select the database
-       let database = client.database(named: "tracker2")
+            // Select the collection
+        let collection = database.collection(withName: "User")
 
-       // Select the collection
-       let collection = database.collection(withName: "User")
+            // Using the user's id to look up tasks
+            let user = app.currentUser!
+            let identity = "user=\(user.id)"
 
-       collection.aggregate( pipeline: [], { (result) in
-           // Note: this completion handler may be called on a background thread.
-           //       If you intend to operate on the UI, dispatch back to the main
-           //       thread with `DispatchQueue.main.async {}`.
-           switch result {
+        collection.aggregate( pipeline: [], { (result) in
+              // Note: this completion handler may be called on a background thread.
+              //       If you intend to operate on the UI, dispatch back to the main
+              //       thread with `DispatchQueue.main.async {}`.
+              switch result {
 
-           case .failure(let error):
-               // Handle errors
-               print("Call to MongoDB failed: \(error.localizedDescription)")
-               return
+              case .failure(let error):
+                  // Handle errors
+                  print("Call to MongoDB failed: \(error.localizedDescription)")
+                  return
 
-           case .success(let documents):
-               // Print each document
-               print("Results: \(documents)")
-               documents.forEach({(document) in
-                   print("Document:")
-                   document.forEach({ (key, value) in
-                       print("  key: \(key), value: \(value)")
-                   })
-               })
-           }
-       })
-   }
-
+              case .success(let documents):
+                  // Print each document
+                  print("Results: \(documents)")
+                  documents.forEach({(document) in
+                      print("Document:")
+                      document.forEach({ (key, value) in
+                          print("  key: \(key), value: \(value)")
+                      })
+                  })
+              }
+          })
+      }
+        
+        
+        
     
     
     
